30101c063d20172eb0d3fc847cc59e26738503f0
Ashwin Rao <ashwin.rao@getcruise.com>
2019-06-19 19:29:48 -0700

    Add positional arguments

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,25 @@ bool parse_args(int argc, char** argv, po::variables_map& args){
 
   // Declare supported options
   po::options_description desc("Allowed options");
-
-  // Add options
   desc.add_options()
-    ("help", "Help message")
-    ("directory,d",po::value<std::string>()->default_value("./"), "Directory")
+    ("help,h", "Help message")
     ("find,f",po::value<std::string>(), "Find pattern")
     ("rename,r",po::value<std::string>(),"Rename pattern")
+    ("directory,d",po::value<std::string>()->default_value("./"), "Directory")
     ("no-confirmation,n", "Do not provide confirmation")
   ;
 
-  po::store(po::parse_command_line(argc,argv,desc),args);
+  // Make options positional
+  po::positional_options_description desc_p;
+  desc_p.add("find",1);
+  desc_p.add("rename",1);
 
+  // Store options
+  po::store(po::command_line_parser(argc,argv).
+            options(desc).positional(desc_p).run(), args);
+  po::notify(args);
+
+  // Handle help
   if(args.count("help")){
     std::cout << desc << std::endl;
     return false;
