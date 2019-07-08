bff4bb8b446da91973bbb240b2a376ad1ee62290
Ashwin <ashwin.rao@getcruise.com>
2019-07-07 19:51:34 -0700

    Add parse args test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -6,6 +6,23 @@ TEST(Main, EqualsOne)
     EXPECT_EQ(1, 1);
 }
 
+TEST(ParseArgsTest,HandleNoArgs){
+
+    // Create argv and argc
+    std::vector<std::string> arguments = {"cren"};
+    std::vector<char*> argv;
+    for (auto& arg: arguments){
+        argv.push_back((char*)(arg.data()));
+    }
+    argv.push_back(nullptr);
+    int argc = argv.size() - 1;
+
+    po::variables_map args;
+
+    // Program shouldn't continue
+    ASSERT_FALSE(parse_args(argc, argv.data(), args));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
